3aad128b9fa786c64860cc784aa5de849a4ac831
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2024-05-04 01:43:50 -0500

    argprintfix

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerClient.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/docker/DockerClient.scala
@@ -164,11 +164,12 @@ class DockerClient(dockerHost: Option[String] = None,
           log.error(this, s"Failed to create network ${networkName}, using bridge network")
           //iterate over runArgs in pairs
           //if the first element is "--network" change the second element to "bridge" in place
-            runArgs = runArgs.sliding(2).flatMap {
+            var tmp = runArgs.sliding(2).flatMap {
               case Seq("--network", _) => Seq("--network", "bridge")
               case other => other
             }.toSeq
-            log.info(this, s"New runArgs: ${runArgs.toString()}")
+            log.info(this, s"New runArgs: ${tmp.toString()}")
+            runArgs = tmp
             Future[String]("bridge")
       }
       //TODO: Actually add bridge network fallback here
